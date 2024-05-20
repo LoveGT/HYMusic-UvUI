12da669667345a844b25481db363f6dfb881b096
gaotao <807643756@qq.com>
2024-05-20 11:31:50 +0800

    feat: 集成@antfu/eslint-config插件

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  formatters: true,
+  vue: true,
+  ignores: ['node_modules', '**/node_modules/**', 'dist', '**/dist/**', '*.css', '**/*.css/**', '*.less', '**/*.less/**', '*.scss', '**/*.scss/**', '*.jpg', '**/*.jpg/**', '*.png', '**/*.png/**', '*.gif', '**/*.gif/**', '*.svg', '**/*.svg/**', '*.html', '**/*.html/**', '*.d.ts', '**/*.d.ts/**'],
+})
