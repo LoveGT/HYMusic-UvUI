cc910112aa70a6742cd901d01826a93d6ec26b7a
gaotao <gaotao@perfect-zx.com>
2024-05-30 00:04:12 +0800

    feat(tabbar): 配置tabbar和搭建相关页面

---
+++ src/pages/home-music/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useAppStore } from '@/store/modules/app'
+
+const appStore = useAppStore()
+
+const videoUrl = ref('1111')
+</script>
+
+<template>
+  <div>視頻 {{ videoUrl }} {{ appStore.count }}</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/home-video/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <view>
+    video..........
+  </view>
+</template>
+
+<style scoped>
+
+</style>
